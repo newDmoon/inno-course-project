23c1e29f683735f15dfb68a5cfd689fd3614e5f9
newDmoon <20125dimanovogrodsky@gmail.com>
2025-11-25 10:14:12 +0300

    PMT-0: deserializer fix

--- payment-service/src/main/java/org/innowise/paymentservice/model/dto/event/OrderCreatedEvent.java
+++ payment-service/src/main/java/org/innowise/paymentservice/model/dto/event/OrderCreatedEvent.java
@@ -1,12 +1,14 @@
 package org.innowise.paymentservice.model.dto.event;
 
-import lombok.Builder;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Data
-@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderCreatedEvent {
     private Long orderId;
     private Long userId;
