c0cec9cafd1008dbb6002ba7780a165a69ea1cd5
newDmoon <20125dimanovogrodsky@gmail.com>
2025-11-30 19:32:47 +0300

    k8s-0: minor fixes

--- payment-service/src/main/java/org/innowise/paymentservice/model/dto/event/OrderCreatedEvent.java
+++ payment-service/src/main/java/org/innowise/paymentservice/model/dto/event/OrderCreatedEvent.java
@@ -1,6 +1,7 @@
 package org.innowise.paymentservice.model.dto.event;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +10,7 @@ import java.math.BigDecimal;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class OrderCreatedEvent {
     private Long orderId;
     private Long userId;
